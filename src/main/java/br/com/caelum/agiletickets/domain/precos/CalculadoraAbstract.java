4bb03cda9172357c87897be9225bdf6de6f73fa9
Felipe Rocha <felipegr6@gmail.com>
2016-03-26 17:24:35 -0300

    Refactor

--- src/main/java/br/com/caelum/agiletickets/domain/precos/CalculadoraAbstract.java
+++ src/main/java/br/com/caelum/agiletickets/domain/precos/CalculadoraAbstract.java
@@ -7,6 +7,10 @@ import br.com.caelum.agiletickets.models.Sessao;
 public abstract class CalculadoraAbstract {
 
 	protected BigDecimal preco;
+	
+	public CalculadoraAbstract(){
+		preco = BigDecimal.ZERO;
+	}
 
 	public abstract BigDecimal calcula(Sessao sessao, Integer quantidade);
 
